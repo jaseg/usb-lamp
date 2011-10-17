c4b981b6b2953f0a8ed22d226e3376102814f0c2
jaseg <s@jaseg.de>
2011-10-17 22:57:14 +0200

    Working to port the stuff...

---
+++ requests.h
@@ -0,0 +1,26 @@
+/* 	Penta host implementation
+	Copyright (C) 2011 by Sebastian Götte <s@jaseg.de>
+
+	This program is free software: you can redistribute it and/or modify
+	it under the terms of the GNU General Public License as published by
+	the Free Software Foundation, either version 3 of the License, or
+	(at your option) any later version.
+
+	This program is distributed in the hope that it will be useful,
+	but WITHOUT ANY WARRANTY; without even the implied warranty of
+	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+	GNU General Public License for more details.
+
+	You should have received a copy of the GNU General Public License
+	along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+//Request ids
+#define LAMP_REQ_SET_LED	1
+#define LAMP_REQ_SET_LED_PRECISE 5
+#define LAMP_REQ_FADE		2
+#define LAMP_REQ_FADE_PRECISE 6
+//For powering on and powering off
+#define LAMP_REQ_POWER		3
+#define LAMP_GET_STATE		4
+
